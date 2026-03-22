ed31d208fffc5994953a76095ba6d06f08ced06e
kothamangalamhaka <kothamangalamhaka@gmail.com>
2026-03-22 19:40:29 +0530

    Delete pwa directory

--- pwa/app.js
+++
@@ -1,8 +0,0 @@
-function openHaka() {
-  window.location.href = "../index.html";
-}
-
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
-}
